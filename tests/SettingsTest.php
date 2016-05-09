e563658268a71c970aeb36bf825fdf7da41ce3fb
Antoine Benevaut <antoine@cvepdb.fr>
2016-05-09 16:59:28 +0200

    Progress on Tests.

--- tests/SettingsTest.php
+++ tests/SettingsTest.php
@@ -12,47 +12,81 @@ use CVEPDB\Settings\Cache;
 class SettingsTest extends PHPUnit_Framework_TestCase
 {
 
+	const SETTINGS_TABLE = 'settings';
+	const SETTINGS_COL_KEY = 'setting_key';
+	const SETTINGS_COL_VAL = 'setting_value';
+
+	/**
+	 * @var
+	 */
 	protected $settings;
+
+	/**
+	 * @var
+	 */
 	protected $db;
+
+	/**
+	 * @var
+	 */
 	protected $config;
 
+	/**
+	 *
+	 */
 	protected function setUp()
 	{
 		$this->db = $this->initDb();
 
 		$this->config = [
-			'db_table'   => 'settings',
+			'db_table'   => self::SETTINGS_TABLE,
 			'cache_file' => storage_path('settings.json'),
 		];
 		$this->settings = new Settings($this->db, new Cache($this->config['cache_file']), $this->config);
 	}
 
-
+	/**
+	 *
+	 */
 	public function testSet()
 	{
-		$this->settings->set('key', 'value');
-
-		$setting = $this->db->table($this->config['db_table'])->where('key', 'key')->first(['value']);
-		$this->assertEquals('value', unserialize($setting['value']));
-	}
-
-	public function testSetArray()
-	{
-		$set = ['valuekey' => 'value'];
+		$set = 'value';
 		$this->settings->set('key', $set);
 
-		$setting = $this->db->table($this->config['db_table'])->where('key', 'key')->first(['value']);
+		$setting = $this->db->table(self::SETTINGS_TABLE)->where(self::SETTINGS_COL_KEY, 'key')->first([self::SETTINGS_COL_VAL]);
 
-		$this->assertEquals($set, unserialize($setting['value']));
+		$this->assertEquals($set, unserialize($setting->{self::SETTINGS_COL_VAL}));
 		$this->assertEquals($set, $this->settings->get('key'));
 	}
 
-	public function testGet()
+	/**
+	 *
+	 */
+	public function testSetArray()
 	{
-		$this->settings->set('key', 'value');
-		$this->assertEquals('value', $this->settings->get('key'));
+		$set = ['key' => 'value'];
+		$this->settings->set('key', $set);
+
+		$setting = $this->db->table(self::SETTINGS_TABLE)->where(self::SETTINGS_COL_KEY, 'key')->first([self::SETTINGS_COL_VAL]);
+
+		$this->assertEquals($set, unserialize($setting->{self::SETTINGS_COL_VAL}));
+		$this->assertEquals($set, $this->settings->get('key'));
 	}
 
+	/**
+	 *
+	 */
+	public function testGet()
+	{
+		$set = 'value';
+		$this->settings->set('key', $set);
+
+		$this->assertEquals($set, $this->settings->get('key'));
+	}
+
+	/**
+	 *
+	 */
 	public function testGetAll()
 	{
 		$this->settings->set('key', 'value');
@@ -64,6 +98,9 @@ class SettingsTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(['key' => 'value', 'key2' => 'value2'], $this->settings->getAll());
 	}
 
+	/**
+	 *
+	 */
 	public function testFlush()
 	{
 		$this->settings->set('key', 'value');
@@ -71,25 +108,37 @@ class SettingsTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals([], $this->settings->getAll());
 	}
 
+	/**
+	 *
+	 */
 	public function testHasKey()
 	{
 		$this->settings->set('key', 'value');
+
 		$this->assertTrue($this->settings->hasKey('key'));
 		$this->assertFalse($this->settings->hasKey('key2'));
 	}
 
+	/**
+	 *
+	 */
 	public function testHasKeyWithoutCache()
 	{
 		$this->settings->set('key', 'value');
+
 		$this->assertTrue($this->settings->hasKey('key'));
 		$this->assertFalse($this->settings->hasKey('key2'));
 
 		@unlink(storage_path('settings.json'));
+
 		$this->assertTrue($this->settings->hasKey('key'));
 		$this->assertFalse($this->settings->hasKey('key2'));
 
 	}
 
+	/**
+	 *
+	 */
 	public function testForget()
 	{
 		$this->settings->set('key', 'value');
@@ -97,13 +146,18 @@ class SettingsTest extends PHPUnit_Framework_TestCase
 		$this->assertNull($this->settings->get('key'));
 	}
 
+	/**
+	 *
+	 */
 	protected function tearDown()
 	{
 		Capsule::schema()->drop('settings');
 		@unlink(storage_path('settings.json'));
 	}
 
-
+	/**
+	 * @return \Illuminate\Database\DatabaseManager
+	 */
 	private function initDb()
 	{
 		$capsule = new Capsule;
@@ -114,14 +168,15 @@ class SettingsTest extends PHPUnit_Framework_TestCase
 			'database' => ':memory:',
 			'prefix'   => '',
 		]);
+
 		$capsule->setEventDispatcher(new Dispatcher(new Container));
 		$capsule->setAsGlobal();
 		$capsule->bootEloquent();
 
 		Capsule::schema()->create('settings', function ($table)
 		{
-			$table->string('key', 100)->index()->unique('key');
-			$table->text('value', 65535)->nullable();
+			$table->string(self::SETTINGS_COL_KEY, 100)->index()->unique('key');
+			$table->text(self::SETTINGS_COL_VAL, 65535)->nullable();
 		});
 
 		return $capsule->getDatabaseManager();
